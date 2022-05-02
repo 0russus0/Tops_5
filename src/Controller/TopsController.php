941e4ad8e05a3eb17f5a5c64dac60555fdc63b99
Russus <pierreetleroux@gmail.com>
2022-05-02 21:52:56 +0200

    [WIP]Start CRUD and maj webpack

--- src/Controller/TopsController.php
+++ src/Controller/TopsController.php
@@ -12,23 +12,46 @@ use Symfony\Component\HttpFoundation\Request;
 class TopsController extends AbstractController
 {
     #[Route('/tops', name: 'app_tops')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine)
     {
+        $rep = $doctrine->getRepository(Top::class);
+        $tops = $rep->findAll();
+        // if using Vue js return a response JS
+        // $response = new Response(json_encode($tops));
+        // $response->headers->set('Content-Type', 'application/json');
+        // return $response;
         return $this->render('tops/index.html.twig', [
-            'controller_name' => 'TopsController',
+            'tops' => $tops,
         ]);
     }
 
-    #[Route('/top/:id', name: 'app_top')]
-    public function show(ManagerRegistry $doctrine, int $id): Response
+    #[Route('/top/{id}', name: 'app_top')]
+    public function show(ManagerRegistry $doctrine, int $id)
     {
         $rep = $doctrine->getRepository(Top::class);
         $top = $rep->find($id);
+        // if using Vue js return a response JS
+        // $response = new Response(json_encode($top->getTitle()));
+        // $response->headers->set('Content-Type', 'application/json');
+        // return $response;
+        return $this->render('tops/show.html.twig', [
+            'top' => $top,
+        ]);
+    }
+
+    #[Route('/top/create', name: 'create_top')]
+    public function create(ManagerRegistry $doctrine, Request $request)
+    {
+        $em = $doctrine->getManager();
+        $top = new Top();
+        $top->setTitle($request->title);
+        $em->persist($top);
+        $em->flush();
         return $top;
     }
 
-    #[Route('/top/add', name: 'add_top')]
-    public function add(ManagerRegistry $doctrine, Request $request)
+    #[Route('/top/edit/{id}', name: 'edit_top')]
+    public function edit(ManagerRegistry $doctrine, Request $request)
     {
         $em = $doctrine->getManager();
         $top = new Top();
